30a2a2c4821bf12d0091b016879c0f2e355ce85c
obkwon <obkwon@da-rum.com>
2022-07-13 11:32:37 +0900

    초기 세팅 수정

--- src/main/resources/db/dangol_mysql_DDL.sql
+++ src/main/resources/db/dangol_mysql_DDL.sql
@@ -89,3 +89,76 @@ CREATE TABLE `stores` (
   `modify_id` varchar(100) NOT NULL COMMENT '수정자',
   PRIMARY KEY (`snum`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+-- dangol.stag definition
+
+CREATE TABLE `stag` (
+  `stnum` int(10) NOT NULL AUTO_INCREMENT COMMENT '가게태그번호',
+  `snum` int(10) NOT NULL COMMENT '가게번호',
+  `anum` int(10) NOT NULL COMMENT '관리번호',
+  `regist_dt` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '등록일자',
+  `modify_dt` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '수정일자',
+  `regist_id` varchar(100) NOT NULL COMMENT '등록자',
+  `modify_id` varchar(100) NOT NULL COMMENT '수정자',  
+  PRIMARY KEY (`stnum`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+-- dangol.grades definition
+
+CREATE TABLE `grades` (
+  `gnum` int(10) NOT NULL AUTO_INCREMENT COMMENT '등급번호',
+  `mid` varchar(20) DEFAULT NULL COMMENT '회원아이디',
+  `snum` int(10) DEFAULT NULL COMMENT '가게번호',
+  `glevel` int(1) DEFAULT '0' COMMENT '등급',
+  `gcurrent` varchar(5) NOT NULL DEFAULT 'N' COMMENT '현재 등급 확인 (Y/N)',
+  `glike` int(1) DEFAULT '0' COMMENT '좋아요',
+  `regist_dt` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '등록일자',
+  `modify_dt` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '수정일자',
+  `regist_id` varchar(100) NOT NULL COMMENT '등록자',
+  `modify_id` varchar(100) NOT NULL COMMENT '수정자',   
+  PRIMARY KEY (`gnum`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+-- dangol.details definition
+
+CREATE TABLE `details` (
+  `dnum` int(10) NOT NULL AUTO_INCREMENT COMMENT '내역번호',
+  `gnum` int(10) DEFAULT NULL COMMENT '등급번호',
+  `ddate` datetime NOT NULL COMMENT '예약날짜 OR 혜택받은 날짜',
+  `dperson` int(10) DEFAULT NULL COMMENT '예약인원',
+  `dmenu` varchar(50) DEFAULT NULL COMMENT '예약메뉴',
+  `dtype` varchar(10) NOT NULL DEFAULT 'N' COMMENT '예약여부 (Y/N)',
+  `dask` varchar(100) DEFAULT NULL COMMENT '요구사항',
+  `dcheck` varchar(5) NOT NULL DEFAULT 'N' COMMENT '결제여부 (Y/N)',
+  `dtime` varchar(15) DEFAULT NULL COMMENT '예약시간',
+  `dcount` int(10) DEFAULT '0' COMMENT '후기횟수',
+  `dlimit` int(10) DEFAULT NULL COMMENT '사용자 예약 시 비교대상이 되는 예약 가능 최대 인원',
+  `regist_dt` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '등록일자',
+  `modify_dt` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '수정일자',
+  `regist_id` varchar(100) NOT NULL COMMENT '등록자',
+  `modify_id` varchar(100) NOT NULL COMMENT '수정자', 
+  PRIMARY KEY (`dnum`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+-- dangol.inquirys definition
+
+CREATE TABLE `inquirys` (
+  `inum` int(10) NOT NULL AUTO_INCREMENT COMMENT '문의번호',
+  `mid` varchar(20) DEFAULT NULL COMMENT '회원아이디',
+  `bid` varchar(20) DEFAULT NULL COMMENT '사장님아이디',
+  `itype` varchar(10) NOT NULL COMMENT '문의유형(SV - service, ST - site)',
+  `ititle` varchar(50) NOT NULL COMMENT '문의제목',
+  `icontent` varchar(150) NOT NULL COMMENT '문의내용',
+  `idate` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '문의날짜',
+  `istate` varchar(5) NOT NULL DEFAULT 'N' COMMENT '답변상태(Y/N)',
+  `ianswer` varchar(150) DEFAULT NULL COMMENT '답변내용',
+  `regist_dt` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '등록일자',
+  `modify_dt` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '수정일자',
+  `regist_id` varchar(100) NOT NULL COMMENT '등록자',
+  `modify_id` varchar(100) NOT NULL COMMENT '수정자', 
+  PRIMARY KEY (`inum`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
